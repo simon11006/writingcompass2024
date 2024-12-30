df04aa33ef9bb81967d86506d9b687e2030a6661
simon11006 <endeavor1006@naver.com>
2024-12-30 17:31:36 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  distDir: '.next'
+  swcMinify: true,
+  output: 'standalone'
 }
 
 module.exports = nextConfig
